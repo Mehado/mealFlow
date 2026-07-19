7bb7059ec26ffde7ae26e7ab34d813f7088fe132
mehado <minhode@outlook.com>
2026-07-19 21:54:00 +0800

    缓存套餐代码开发

--- sky-server/src/main/java/com/sky/controller/admin/SetmealController.java
+++ sky-server/src/main/java/com/sky/controller/admin/SetmealController.java
@@ -10,6 +10,8 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -32,6 +34,7 @@ public class SetmealController {
      */
     @PostMapping
     @Operation(summary = "新增套餐")
+    @CacheEvict(cacheNames="setmealCache",key="#setmealDTO.categoryId")
     public Result save(@RequestBody SetmealDTO setmealDTO){
         log.info("新增套餐:{}",setmealDTO);
         setmealService.saveWithDish(setmealDTO);
@@ -55,6 +58,7 @@ public class SetmealController {
      */
     @DeleteMapping
     @Operation(summary = "根据id批量删除套餐")
+    @CacheEvict(cacheNames="setmealCache",allEntries = true)
     public Result delete(@RequestParam List<Long> ids){
         log.info("根据id批量删除套餐:{}",ids);
         setmealService.deleteBatch(ids);
@@ -79,6 +83,7 @@ public class SetmealController {
      */
     @PutMapping
     @Operation(summary = "修改套餐")
+    @CacheEvict(cacheNames="setmealCache",allEntries = true)
     public Result update(@RequestBody SetmealDTO setmealDTO) {
         log.info("修改套餐:{}", setmealDTO);
         setmealService.update(setmealDTO);
@@ -92,6 +97,7 @@ public class SetmealController {
      */
     @PostMapping("/status/{status}")
     @Operation(summary = "套餐起售停售")
+    @CacheEvict(cacheNames="setmealCache",allEntries = true)
     public Result startOrStop(@PathVariable Integer status,Long id){
         log.info("套餐起售停售:status={},id={}",status,id);
         setmealService.startOrStop(status,id);
--- sky-server/src/main/java/com/sky/controller/user/SetmealController.java
+++ sky-server/src/main/java/com/sky/controller/user/SetmealController.java
@@ -8,6 +8,7 @@ import com.sky.vo.DishItemVO;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -27,6 +28,7 @@ public class SetmealController {
      * @param categoryId
      * @return
      */
+    @Cacheable(cacheNames = "setmealCache", key = "#categoryId")//key::setmealCache::categoryId
     @GetMapping("/list")
     @Operation(summary = "根据分类id查询套餐")
     public Result<List<Setmeal>> list(Long categoryId) {
